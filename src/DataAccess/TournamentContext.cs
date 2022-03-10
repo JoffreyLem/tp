bac4766eb14a82d2c0e1f2a08196ff0071e0921c
Joffrey <lemery.joffrey@outlook.fr>
2022-03-10 10:05:05 +0100

    ajout entity framework

---
+++ src/DataAccess/TournamentContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tournaments.DataAccess
+{
+    public class TournamentContext : DbContext
+    {
+        public TournamentContext()
+
+        {
+
+        }
+        public TournamentContext(DbContextOptions<TournamentContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=Tournament;Trusted_Connection=True;");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
